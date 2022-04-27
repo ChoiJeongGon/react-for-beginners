b943a99562647b1e8c0d6129bb72e31da116c8be
ChoinJeongGon <shinbaji@gmail.com>
2022-04-27 19:45:05 +0900

    useEffcets Upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
-  useEffect(() => {
-    console.log("Call the API...");
-  }, []);
-
-  return (
-   <div>
-     <h1>{counter}</h1>
-     <button onClick={onClick}>click me</button>
-     </div>
-  );
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("")
+    const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (event) => setKeyword(event.target.value);
+    useEffect(() => {
+        console.log("I run only once.");
+    }, []);
+    useEffect(() => {
+      console.log("I run when 'keyword' changes.");
+    }, [keyword]);
+    useEffect(() => {
+      console.log("I run when 'counter' changes.");
+    }, [counter]);
+    useEffect(() =>{
+      console.log("I run wher keyword & counter change.");
+    }, [keyword, counter]);
+    return (
+        <div>
+            <input
+                onChange={onChange}
+                value={keyword}
+                type="text"
+                placeholder="Search here..."/>
+            <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button>
+        </div>
+    );
 }
 
 export default App;
