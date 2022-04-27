94ba44b4e3e77c6c2b73b5176b60d2b1b2e6b028
ChoinJeongGon <shinbaji@gmail.com>
2022-04-27 20:13:04 +0900

    useEffect Upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import {useState, useEffect} from "react";
 
+function Hello() {
+
+    // useEffect()
+    useEffect(() => {
+        console.log("hi :) ");
+        return() => console.log("bye :( ");
+    }, [])
+    
+    // function()
+    useEffect(function () {
+        console.log("hi :) ");
+        return function () {
+            console.log("bye :( ");
+        }
+    }, []);
+    return <h1>Hello</h1>;
+}
 function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("")
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-    useEffect(() => {
-        console.log("I run only once.");
-    }, []); // 한번만 실행
-    useEffect(() => {
-      console.log("I run when 'keyword' changes.");
-    }, [keyword]); // keyword 변화시 계속 실행
-    useEffect(() => {
-      console.log("I run when 'counter' changes.");
-    }, [counter]); // counter 변화시 계속 실행
-    useEffect(() =>{
-      console.log("I run wher keyword & counter change.");
-    }, [keyword, counter]); // keyword, counter 변화시 계속 실행
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
     return (
         <div>
-            <input
-                onChange={onChange}
-                value={keyword}
-                type="text"
-                placeholder="Search here..."/>
-            <h1>{counter}</h1>
-            <button onClick={onClick}>click me</button>
+            {
+                showing
+                    ? <Hello/>
+                    : null
+            }
+            <button onClick={onClick}>{
+                    showing
+                        ? "Hide"
+                        : "Show"
+                }</button>
         </div>
     );
 }
