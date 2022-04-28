07185e5f99b8ad5304ae1421d98d80e7aa2c34ab
ChoinJeongGon <shinbaji@gmail.com>
2022-04-28 15:07:57 +0900

    To Do list Upload

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
-import {useState, useEffect} from "react";
+import {useState} from "react";
 
-function Hello() {
-
-    // useEffect()
-    useEffect(() => {
-        console.log("hi :) ");
-        return() => console.log("bye :( ");
-    }, [])
-    
-    // function()
-    useEffect(function () {
-        console.log("hi :) ");
-        return function () {
-            console.log("bye :( ");
-        }
-    }, []);
-    return <h1>Hello</h1>;
-}
 function App() {
-    const [showing, setShowing] = useState(false);
-    const onClick = () => setShowing((prev) => !prev);
-    return (
-        <div>
-            {
-                showing
-                    ? <Hello/>
-                    : null
-            }
-            <button onClick={onClick}>{
-                    showing
-                        ? "Hide"
-                        : "Show"
-                }</button>
-        </div>
-    );
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDo("");
+        setToDos(currentArray => [toDo, ]);
+    };
+
+    return <div>
+        <form onSubmit={onSubmit}>
+            <input
+                onChange={onChange}
+                value={toDo}
+                type="text"
+                placeholder="Write your to do..."/>
+            <button>Add To Do</button>
+        </form>
+    </div>;
 }
 
 export default App;
