94c1fbd237821fa213116c8cc87a046870f4cf31
Choi Jeong Gon <shinbaji@gmail.com>
2022-05-01 23:02:58 +0900

    React Router Upload

--- src/routes/Detall.js
+++
@@ -1,4 +0,0 @@
-function Detall(){
-    return <h1>Detall</h1>
-}
-export default Detall;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
 
 function Home(){
-    const [loading, setLoading] = useStatetate(true);
+    const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
-    async function getMovies() {
-      const response = await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      );
-      const json = await response.json();
+    const getMovies = async () => {
+      const json = await (
+        await fetch(
+          `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+        )
+      ).json();
       setMovies(json.data.movies);
       setLoading(false);
     }
